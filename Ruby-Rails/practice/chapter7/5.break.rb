b055f4c6025a0ae007b4dc4be277b85c2f0ddf96
matthewkendrick <k.42pd@icloud.com>
2022-01-11 20:46:25 +0900

    [Finish]Ruby,practice-chapter7

--- Ruby-Rails/practice/chapter7/5.break.rb
+++ Ruby-Rails/practice/chapter7/5.break.rb
@@ -1,1 +1,9 @@
+i = 2
 
+while i <= 10 do
+  if i == 5
+    puts "処理を終了します"
+    break
+  end
+  puts i += 1
+end
---
+++ Ruby-Rails/practice/chapter7/7.lesson7-2.rb
@@ -0,0 +1,27 @@
+puts "計算をはじめます\n何回計算を繰り返しますか？"
+#繰り返す回数 = repeat
+i = 0
+repeat = gets.to_i
+
+while i <= repeat do
+  i += 1
+  if i > repeat
+    puts "計算を終了します"
+    break
+  end
+  puts i.to_s + "回目の計算\n2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a=" + a.to_s + "\nb=" + b.to_s
+
+  num1 = a + b
+  num2 = a - b
+  num3 = a * b
+  num4 = a / b
+
+  puts "計算結果を出力します" +
+         "\na+b=" + num1.to_s +
+         "\na-b=" + num2.to_s +
+         "\na*b=" + num3.to_s +
+         "\na/b=" + num4.to_s
+end
